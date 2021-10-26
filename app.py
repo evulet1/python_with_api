94b4e8681f5ba4de2751d27c89d9dab6588825b1
Evulet1 <elvirochkata@gmail.com>
2021-10-26 17:59:46 +0300

    creating lib

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import requests
+from libs.openexchange import OpenExchangeClient
 
 APP_ID = '5e00eba3300b4b11923e4b6c3197503a'
-ENDPINT = 'https://openexchangerates.org/api/latest.json'
 
-response = requests.get(f'{ENDPINT}?app_id={APP_ID}')
-exchange_rates = response.json()['rates']
+client = OpenExchangeClient(APP_ID)
 
 usd_amount = 1000
-gbp_amount = usd_amount * exchange_rates['GBP']
+gbp_amount = client.convert(usd_amount, 'USD', 'GBP')
 
-print(f'USD{usd_amount} is GBP{gbp_amount}')
+print(f'USD{usd_amount} is GBP{gbp_amount:.2f}')
